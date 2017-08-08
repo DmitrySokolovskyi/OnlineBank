2fb823c14b25c370ef04fcff4b0a504c4a9e09ab
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-08-08 18:54:38 +0300

    created profile

---
+++ src/main/java/com/onlinebank/web/UserController.java
@@ -0,0 +1,47 @@
+package com.onlinebank.web;
+
+import com.onlinebank.domain.User;
+import com.onlinebank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/profile", method = GET)
+    public String profile(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+
+        model.addAttribute("user", user);
+
+        return "profile";
+    }
+
+    @RequestMapping(value = "/profile", method = POST)
+    public String profilePost(@ModelAttribute("user") User newUser, Model model) {
+        User user = userService.findByUsername(newUser.getUsername());
+        user.setUsername(newUser.getUsername());
+        user.setFirstName(newUser.getFirstName());
+        user.setLastName(newUser.getLastName());
+        user.setEmail(newUser.getEmail());
+        user.setPhone(newUser.getPhone());
+
+        model.addAttribute("user", user);
+
+        userService.saveUser(user);
+
+        return "profile";
+    }
+}
